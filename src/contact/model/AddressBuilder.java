f250357740b98aa148537ee2d5846c02bee619bc
Sinan Ö <46645793+SaveEnergy@users.noreply.github.com>
2019-02-08 16:39:31 +0100

    Implementing Builder

--- src/contact/model/AddressBuilder.java
+++ src/contact/model/AddressBuilder.java
@@ -2,4 +2,47 @@ package contact.model;
 
 public class AddressBuilder {
 
+  private String country = "";
+  private String city = "";
+  private String zipCode = "";
+  private String street = "";
+  private String streetNumber = "";
+
+  private AddressBuilder() {
+
+  }
+
+  public AddressBuilder getAddressBuilder() {
+    return new AddressBuilder();
+  }
+
+  AddressBuilder getCountry(String country) {
+    this.country = country;
+    return this;
+  }
+
+  AddressBuilder getCity(String city) {
+    this.city = city;
+    return this;
+  }
+
+  AddressBuilder getZipCode(String zipCode) {
+    this.zipCode = zipCode;
+    return this;
+  }
+
+  AddressBuilder getStreet(String street) {
+    this.street = street;
+    return this;
+  }
+
+  AddressBuilder getStreetNumber(String streetNumber) {
+    this.streetNumber = streetNumber;
+    return this;
+  }
+
+  Address buildAddress() {
+    return new Address(country, city, zipCode, street, streetNumber);
+  }
+
 }
--- src/contact/model/CommunicationBuilder.java
+++ src/contact/model/CommunicationBuilder.java
@@ -2,4 +2,24 @@ package contact.model;
 
 public class CommunicationBuilder {
 
+  private String phone;
+  private String mail = "";
+
+  private CommunicationBuilder(String phone) {
+    this.phone = phone;
+  }
+
+  public CommunicationBuilder getCommunicationBuilder(String phone) {
+    return new CommunicationBuilder(phone);
+  }
+
+  CommunicationBuilder setMail(String mail) {
+    this.mail = mail;
+    return this;
+  }
+
+  Communication buildComunication() {
+    return new Communication(phone, mail);
+  }
+
 }

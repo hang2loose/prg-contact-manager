42cc1c2baef79348fc3a36b66cf28f66af22cd2b
Rauch Moritz <moritz.rauch@cargarantie.com>
2019-02-08 16:22:57 +0100

    added constructors

--- src/contact/model/Address.java
+++ src/contact/model/Address.java
@@ -8,4 +8,12 @@ public class Address {
   private String street;
   private String streetNumber;
 
+  Address(String country, String city, String zipCode, String street,
+      String streetNumber) {
+    this.country = country;
+    this.city = city;
+    this.zipCode = zipCode;
+    this.street = street;
+    this.streetNumber = streetNumber;
+  }
 }
--- src/contact/model/Communication.java
+++ src/contact/model/Communication.java
@@ -5,4 +5,8 @@ public class Communication {
   private String phone;
   private String mail;
 
+  Communication(String phone, String mail) {
+    this.phone = phone;
+    this.mail = mail;
+  }
 }

d5c1492dc44ac0df844d602575931bdb8cb40baa
Rauch Moritz <moritz.rauch@cargarantie.com>
2019-02-08 16:41:25 +0100

    added constructors

--- src/Application.java
+++ src/Application.java
@@ -1,14 +1,13 @@
+import contact.ContactManager;
+
 public class Application {
 
   /*static {
     DummyDataCreator.createDummyContacts();
   }
+*/
 
   public static void main(String[] args) {
-    ContactManager.run();
-  }*/
-
-  public static void main(String[] args) {
-    System.out.println("Hello from the evil contact managaer");
+    ContactManager.runContactManager();
   }
 }
--- src/contact/ContactManager.java
+++ src/contact/ContactManager.java
@@ -3,20 +3,57 @@ package contact;
 import java.util.HashMap;
 
 enum StateOfManager {
+  GET_COMMAND
 }
 
 public class ContactManager {
 
+  private static ContactManager contactManager = null;
+
+  public static ContactManager getInstance() {
+
+    if (contactManager == null) {
+      System.out
+          .println("########################################################################");
+      System.out
+          .println("#                         Contact Manager v1.0                         #");
+      System.out
+          .println("########################################################################");
+      System.out.println("\n");
+      contactManager = new ContactManager();
+    }
+    return contactManager;
+  }
+
   HashMap<Integer, ContactCard> contactCardRepo = new HashMap<>();
 
-  private StateOfManager stateOfManager;
+  private static StateOfManager stateOfManager = StateOfManager.GET_COMMAND;
 
-  public boolean runContactManager() {
+  public static boolean runContactManager() {
     while (true) {
       switch (stateOfManager) {
+        case GET_COMMAND:
+          prtintCommands();
+          clear();
+          break;
         default:
           return false;
       }
+    }
+  }
+
+  private static void prtintCommands() {
+    System.out.println("[A] Alle Kontakte anzeigen");
+    System.out.println("[N] Neuen Kontakt erstellen");
+    System.out.println("[K] Kontakt bearbeiten");
+    System.out.println("[D] Kontaktdetails erstellen");
+    System.out.println("[L] Kontakt loeschen");
+    System.out.println("[B] Beenden");
+  }
+
+  private static void clear() {
+    for (int i = 0; i < 5; i++) {
+      System.out.println();
     }
   }
 }

8b443b48195eee9acaafbdab64bb2ab4c83221d0
Rauch Moritz <moritz.rauch@cargarantie.com>
2019-02-09 14:37:29 +0100

    moved addressbuilder to address as innerclass

--- src/contact/services/ContactCardService.java
+++ src/contact/services/ContactCardService.java
@@ -1,5 +1,15 @@
 package contact.services;
 
+import contact.repository.ContactRepository;
+
+// TODO implement as Singleton
 public class ContactCardService {
+
+  private ContactRepository contactRepository;
+
+  public ContactCardService(ContactRepository contactRepository) {
+    this.contactRepository = contactRepository;
+  }
+
 
 }

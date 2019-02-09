d7a9219b70528bba64d8c0d59e28b52a7b757105
Rauch Moritz <moritz.rauch@cargarantie.com>
2019-02-09 15:51:57 +0100

    added getAll and saveALL in Repo

--- src/contact/repository/ContactRepository.java
+++ src/contact/repository/ContactRepository.java
@@ -1,11 +1,17 @@
 package contact.repository;
 
 import contact.model.ContactCard;
+import java.util.Collection;
+import java.util.List;
 import java.util.UUID;
 
 public interface ContactRepository {
 
   ContactCard getContactByID(UUID id);
 
+  Collection<ContactCard> getAllContacts();
+
   void save(ContactCard contactCard);
+
+  void saveAll(List<ContactCard> contactCardList);
 }
--- src/contact/repository/ContactRepositoryImpl.java
+++ src/contact/repository/ContactRepositoryImpl.java
@@ -1,7 +1,9 @@
 package contact.repository;
 
 import contact.model.ContactCard;
+import java.util.Collection;
 import java.util.HashMap;
+import java.util.List;
 import java.util.UUID;
 
 public class ContactRepositoryImpl implements ContactRepository {
@@ -16,5 +18,16 @@ public class ContactRepositoryImpl implements ContactRepository {
   @Override
   public void save(ContactCard contactCard) {
     contactCardMap.put(contactCard.getUID(), contactCard);
+  }
+
+  @Override
+  public void saveAll(List<ContactCard> contactCardList) {
+    contactCardList
+        .forEach(contactCard -> contactCardMap.put(contactCard.getUID(), contactCard));
+  }
+
+  @Override
+  public Collection<ContactCard> getAllContacts() {
+    return contactCardMap.values();
   }
 }
